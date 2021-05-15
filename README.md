e4c4282ddbc0ef858b49b6e19bbc29dd5b186101
Syed Yousuf <smyousuf@ryerson.ca>
2021-05-15 17:21:18 -0400

    iterates through all the entries

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # drawing-boxes-on-images-python
 The python script takes in a csv file and png image as input and outputs an image with the boxes drawn as outlined by the csv file
+
+Required Libraries:
+-Pillow (PIL)
+
+INPUT:
+-jpg image called "sample.jpg"
+
+OUTPUT:
+-outputs a jpg image called "test.jpg"
--- convert.py
+++ convert.py
@@ -5,15 +5,17 @@ from PIL import Image, ImageDraw
 with open('sample.csv') as csvDataFile:
     data=list(csv.reader(csvDataFile))
 
-print(data[1][0])
-box_values = json.loads(data[1][0])
-x1_value = box_values.get('x1')
-y1_value = box_values.get('y1')
-x2_value = box_values.get('x2')
-y2_value = box_values.get('y2')
+#initialize the sample jpg image
+image = Image.open("sample.jpg")
 
-# Draw a box over the specified coordinates
-image = Image.new("RGB", (800, 800), "blue")
-draw = ImageDraw.Draw(image)
-draw.rectangle((x1_value, y1_value, x2_value, y2_value), fill=None, outline="yellow",width=3)
+for index in range(1, len(data)): 
+    box_values = json.loads(data[index][0])
+    x1_value = box_values.get('x1')
+    y1_value = box_values.get('y1')
+    x2_value = box_values.get('x2')
+    y2_value = box_values.get('y2')
+    # Draw a box over the specified coordinates
+    draw = ImageDraw.Draw(image)
+    draw.rectangle((x1_value, y1_value, x2_value, y2_value), fill=None, outline="yellow",width=3)
+
 image.save("test.jpg")
