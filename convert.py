c14471a26006767b30be9af748d267a1f76f9cbc
Syed Yousuf <smyousuf@ryerson.ca>
2021-05-15 15:55:24 -0400

    obtains the x value for the first entry

--- convert.py
+++ convert.py
@@ -1,6 +1,9 @@
 import csv
-import pandas as pd
+import json
 
 with open('sample.csv') as csvDataFile:
     data=list(csv.reader(csvDataFile))
-print(data)
+
+print(data[1][0])
+x_value = json.loads(data[1][0])
+print(x_value.get('x'))
